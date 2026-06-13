782646ca8fd3257ec5f084c9bd235120b1690786
Putra Adi Wijaya <putra@belajar.id>
2026-06-13 10:50:38 +0700

    first commit

---
+++ main.php
@@ -0,0 +1,42 @@
+<?php
+function tampilSalam($nama)
+{
+    return "Halo, " . $nama . "!";
+}
+
+echo tampilSalam("Putra");
+
+function tambah($a, $b)
+{
+    return $a + $b;
+}
+
+echo tambah(10, 5);
+
+function luasPersegi($sisi)
+{
+    return $sisi * $sisi;
+}
+
+echo luasPersegi(4);
+
+
+function cekGenapGanjil($angka)
+{
+    if ($angka % 2 == 0) {
+        return "Genap";
+    }
+
+    return "Ganjil";
+}
+
+echo cekGenapGanjil(7);
+
+function ubahKapital($teks)
+{
+    return strtoupper($teks);
+}
+
+echo ubahKapital("belajar php");
+?>
+
